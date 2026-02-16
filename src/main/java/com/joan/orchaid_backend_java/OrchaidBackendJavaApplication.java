6630bb20ca651cd2728c474dee558ff72dc75f62
Joan <jc7santosba@gmail.com>
2026-02-16 02:09:38 +0100

    feat: bootstrap Spring Boot backend + health endpoint

---
+++ src/main/java/com/joan/orchaid_backend_java/OrchaidBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.joan.orchaid_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchaidBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchaidBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joan/orchaid_backend_java/OrchaidBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joan.orchaid_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchaidBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
